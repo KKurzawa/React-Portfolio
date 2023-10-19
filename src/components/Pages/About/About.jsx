91f850a82c7b9456f0287bef77ded66d1f9aae23
Kris Kurzawa <kurzawa1@yahoo.com>
2023-10-19 18:42:51 -0400

    updated About and App

--- src/components/Pages/About/About.jsx
+++ src/components/Pages/About/About.jsx
@@ -1,1 +1,17 @@
-// Placeholder
+// import { useState } from 'react';
+// import Header from '../../Header';
+// import Footer from '../../Footer';
+// import Navigation from '../../Navigation';
+
+function AboutMe() {
+
+    return (
+        <div>
+            <h1>About Me</h1>
+            <p>I am a full stack developer.</p>
+        </div>
+    );
+}
+
+export default AboutMe;
+
