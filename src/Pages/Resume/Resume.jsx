a56616075f0b6f1ec8ccdb157e8b02a9f8ce294a
Kris Kurzawa <kurzawa1@yahoo.com>
2023-10-20 20:28:24 -0400

    struggling with resume

--- src/Pages/Resume/Resume.jsx
+++ src/Pages/Resume/Resume.jsx
@@ -1,5 +1,13 @@
+// import Resume1 from '../../assets/'
 export default function Resume() {
     return (
-        <h1>Resume</h1>
+        <>
+            <h1>Resume</h1>
+            {/* <Resume1 /> */}
+
+
+
+        </>
+
     )
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,89 @@
 // links to github, linkedin, stack overflow
+import { AiFillGithub } from 'react-icons/ai';
+import { FaLinkedinIn } from 'react-icons/fa';
+import { DiStackoverflow } from 'react-icons/di';
 export default function Footer() {
 
     return (
         <footer>
             <nav>
                 <div>
-                    <a href="https://github.com/KKurzawa">Github</a>
+                    <a href="https://github.com/KKurzawa"><AiFillGithub /></a>
                 </div>
                 <div>
-                    <a href="https://www.linkedin.com/in/kris-kurzawa-34105226b/">LinkedIn</a>
+                    <a href="https://www.linkedin.com/in/kris-kurzawa-34105226b/"><FaLinkedinIn /></a>
                 </div>
                 <div>
-                    <a href="">Stack Overflow</a>
+                    <a href=""><DiStackoverflow /></a>
                 </div>
             </nav>
         </footer>
     );
 }
+
+// import React from "react";
+// import { Container, Row, Col } from "react-bootstrap";
+
+
+// function Footer() {
+//   let date = new Date();
+//   let year = date.getFullYear();
+//   return (
+//     <Container fluid className="footer">
+//       <Row>
+//         <Col md="4" className="footer-copywright">
+//           <h3>Designed and Developed by Soumyajit Behera</h3>
+//         </Col>
+//         <Col md="4" className="footer-copywright">
+//           <h3>Copyright © {year} SB</h3>
+//         </Col>
+//         <Col md="4" className="footer-body">
+//           <ul className="footer-icons">
+//             <li className="social-icons">
+//               <a
+//                 href="https://github.com/soumyajit4419"
+//                 style={{ color: "white" }}
+//                 target="_blank" 
+//                 rel="noopener noreferrer"
+//               >
+//                 <AiFillGithub />
+//               </a>
+//             </li>
+//             <li className="social-icons">
+//               <a
+//                 href="https://twitter.com/Soumyajit4419"
+//                 style={{ color: "white" }}
+//                 target="_blank" 
+//                 rel="noopener noreferrer"
+//               >
+//                 <AiOutlineTwitter />
+//               </a>
+//             </li>
+//             <li className="social-icons">
+//               <a
+//                 href="https://www.linkedin.com/in/soumyajit4419/"
+//                 style={{ color: "white" }}
+//                 target="_blank" 
+//                 rel="noopener noreferrer"
+//               >
+//                 <FaLinkedinIn />
+//               </a>
+//             </li>
+//             <li className="social-icons">
+//               <a
+//                 href="https://www.instagram.com/soumyajit4419"
+//                 style={{ color: "white" }}
+//                 target="_blank" 
+//                 rel="noopener noreferrer"
+//               >
+//                 <AiFillInstagram />
+//               </a>
+//             </li>
+//           </ul>
+//         </Col>
+//       </Row>
+//     </Container>
+//   );
+// }
+
+// export default Footer;
