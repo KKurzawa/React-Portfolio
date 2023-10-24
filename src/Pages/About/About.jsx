2b246399576e1980c7fa95a7dc375cf46115d12d
Kris Kurzawa <kurzawa1@yahoo.com>
2023-10-23 20:41:07 -0400

    responsive design finished other then profile pic

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -21,7 +21,7 @@ export default function AboutMe() {
 
         <div className="about-me">
             <h1>About Me</h1>
-            <img src='../../../public/KrisKurzawaPhoto.jpeg'></img>
+            <img className='profile-pic' src='/KrisKurzawaPhoto.jpeg'></img>
             <h2 className="about-me-body">I am an a full-stack web developer with demonstrated experience across the software development life cycle with an emphasis in JavaScript as my primary object oriented language. I am a lifelong musician/music educator who has performed all over the world and is currently on faculty in the Jazz Department at Wayne State University.</h2>
         </div>
     );
