3391bc097e0cd726593d2c36345e81d8880dc872
Kris Kurzawa <kurzawa1@yahoo.com>
2023-10-24 11:49:54 -0400

    navigation update

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -32,7 +32,7 @@ export default function NavTabs() {
                     // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
                     className={currentPage === '/Contact' ? 'nav-link active' : 'nav-link'}
                 >
-                    Contact
+                    Contact Me
                 </Link>
             </nav>
             <nav className="nav-item">
