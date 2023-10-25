bd4e346577fbb3a2449314341b21168795b3da1d
Kris Kurzawa <kurzawa1@yahoo.com>
2023-10-25 13:02:14 -0400

    css and responsive improvements

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ export default function Header() {
     return (
         <header className="header">
             <div className="header-text">
-                <h1 className="header-name">Kris Kurzawa</h1>
+                <h1 className="header-name">Kris <span className="Kurzawa">Kurzawa </span></h1>
                 <h2 className="header-title">Full-Stack Developer</h2>
             </div>
 
